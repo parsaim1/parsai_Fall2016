bab89ffa70d883ec97d281a2d231e28747bb8244
parsaim1 <parsaim123@gmail.com>
2016-08-30 09:30:53 -0700

    Temp Conversion

---
+++ Temp Conversion/src/TempConvertCode.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Parsa
+ *Date: 08/30/16 
+ *The code converts a temperature in Fahrenheit into Celsius 
+ */
+public class TempConvertCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double F = 32;
+		double C = 5 / 9 * (F - 32);
+		System.out.println(C);	
+
+	}
+
+}
