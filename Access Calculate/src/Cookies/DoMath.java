257e8046dc09681bdd3181b14d2394146e5927ca
parsaim1 <parsaim123@gmail.com>
2016-09-06 09:27:18 -0700

    1.2 Nothing

--- Access Calculate/src/Cookies/DoMath.java
+++ Access Calculate/src/Cookies/DoMath.java
@@ -1,9 +1,20 @@
+/**
+ * 
+ */
+package Cookies;
 
+/**
+ * @author APCS1
+ *
+ */
 public class DoMath {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Calculate.square(5));
+
 	}
 
 }
